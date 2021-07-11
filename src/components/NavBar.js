1a62dc81ebf8d93ee6ea1b3254aade2ed607233a
christiangerbig <chr_gerbig@web.de>
2021-07-11 21:51:02 +0200

    classes to functions + hooks 2.2

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,65 +1,55 @@
-import React, {Component} from "react";
+import React, {useEffect} from "react";
 import {Navbar, Nav} from "react-bootstrap";
 import {Link} from "react-router-dom";
 import {animateScroll as scroll} from "react-scroll";
 
-class NavBar extends Component {
-  
-  constructor(props) {
-    super(props);
-    this.state = {intervalId: null};
-  }
-  
-  componentDidMount() {
-    const intervalId = setInterval(
-      () => this.props.onCheckRequests(), 
-      10000 // check every minute
-    );
-    this.setState({intervalId: intervalId});
-  }
+const NavBar = ({user, newRequestsReceived, headerHeight, introHeight, onCheckRequests}) => {
+  useEffect(
+    () => {
+      const intervalId =  setInterval(
+        () => onCheckRequests(), 
+        10000 // check every minute
+      );
+      return () => clearInterval(intervalId);
+    },
+    []
+  );
 
-  componentWillUnmount() {
-    clearInterval(this.state.intervalId);
-  }
-
-  render() {
-    const {user, newRequestsReceived, headerHeight, introHeight} = this.props;
-      return (
-      <div>
-        <Navbar className="pl-5" variant="dark" expand="lg" fixed="top">
-          <Navbar.Brand href="/"> JungleSwap </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <div>
-                <Link to="/" className="p-2" onClick={() => scroll.scrollTo(headerHeight+introHeight)}> All Plants </Link>
-              </div>
-              {
-                (user) ? (
-                  <div>
-                    <Link className="p-2" to="/plants/create"> Create Plant </Link>
-                    <Link className={(newRequestsReceived) ? "p-2 alertColor" : "p-2"} to="/requests/fetch" title={(newRequestsReceived) ? "new message" : null}> Messages </Link>
-                  </div>
-                ) : null
-              }
-              {
-                (user) ? (
-                  <div>
-                    <Link className="p-2" to="/logout" title={user.username}> Log out </Link>
-                  </div>
-                ) : (
-                  <div>
-                    <Link className="p-2" to="/signin"> Sign in </Link>
-                    <Link className="p-2" to="/signup"> Sign up </Link>
-                  </div>
-                )
-              }
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Navbar className="pl-5" variant="dark" expand="lg" fixed="top">
+        <Navbar.Brand href="/"> JungleSwap </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <div>
+              <Link to="/" className="p-2" onClick={() => scroll.scrollTo(headerHeight+introHeight)}> All Plants </Link>
+            </div>
+            {
+              user ? (
+                <div>
+                  <Link className="p-2" to="/plants/create"> Create Plant </Link>
+                  <Link className={newRequestsReceived ? "p-2 alertColor" : "p-2"} to="/requests/fetch" title={newRequestsReceived ? "new message" : null}> Messages </Link>
+                </div>
+              ) : null
+            }
+            {
+              user ? (
+                <div>
+                  <Link className="p-2" to="/logout" title={user.username}> Log out </Link>
+                </div>
+              ) : (
+                <div>
+                  <Link className="p-2" to="/signin"> Sign in </Link>
+                  <Link className="p-2" to="/signup"> Sign up </Link>
+                </div>
+              )
+            }
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
 }
 
 export default NavBar;
