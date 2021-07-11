80286d4c82e94e6d203c9a76abfd8c36694e167f
christiangerbig <chr_gerbig@web.de>
2021-07-11 13:29:52 +0200

    classes to functions + hooks 1.7

--- src/components/CreatePlantForm.js
+++ src/components/CreatePlantForm.js
@@ -1,8 +1,50 @@
-import React, {Component} from "react";
+import React, {useEffect} from "react";
 import {Link, Redirect} from "react-router-dom";
 import {animateScroll as scroll} from "react-scroll";
 
-class CreatePlantForm extends Component {
+const CreatePlantForm = ({user, headerHeight, introHeight, error, onCreatePlant}) => {
+  useEffect(
+    () => {
+      this.props.onResetError();
+      scroll.scrollToTop();
+    },
+    []
+  );
+
+  if (!user) return (<Redirect to={"/signup"}/>);
+  return (
+    <div className="container row mt-5 fullscreen">
+      <div className="mt-5 col-11 col-md-5 offset-1 offset-md-6">
+        <h2 className="mb-5"> Create a plant </h2>
+        <form onSubmit={onCreatePlant}>
+          <input className="mb-4" name="name" type="text" placeholder="Enter name"/>
+          <input className="mb-4" name="description" type="text" placeholder="Enter description"/>
+          <input className="mb-4 smallWidth" name="size" type="number" min="1" placeholder="Size"/> cm <br/>
+          <select className="mb-4 p-1" name="location" type="text">
+            <option> Select location </option>
+            <option value="sun"> sun </option>
+            <option value="shade"> shade </option>
+            <option value="sun and shade"> sun and shade </option>
+          </select>
+          <br/>
+          <input className="mb-4 smallWidth" name="price" type="number" min="1" placeholder="Price"/> € <br/>
+          <input className="mb-4" name="plantImage" type="file"/>
+          {
+            error ? <p className="warningColor"> {error} </p> : null            
+          }
+          <div className="col-12">
+            <button className="btn btn-sm btn-outline-dark" type="submit" > Create </button>
+            <Link to={"/"} onClick={() => scroll.scrollTo(headerHeight+introHeight)}> <button className="btn btn-sm mx-5"> Go back </button> </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+
+
+/*class CreatePlantForm extends Component {
 
   componentDidMount() {
     this.props.onResetError();
@@ -41,6 +83,6 @@ class CreatePlantForm extends Component {
       </div>
     );
   }
-}
+}*/
 
 export default CreatePlantForm;
