aa7d9442bd29538372ec4c052f31c299055bb0bd
christiangerbig <chr_gerbig@web.de>
2021-07-11 13:51:30 +0200

    classes to functions + hooks 1.9

--- src/components/CreatePlantForm.js
+++ src/components/CreatePlantForm.js
@@ -42,47 +42,4 @@ const CreatePlantForm = ({user, headerHeight, introHeight, error, onCreatePlant,
   );
 }
 
-
-
-/*class CreatePlantForm extends Component {
-
-  componentDidMount() {
-    this.props.onResetError();
-    scroll.scrollToTop();
-  }
-
-  render() {
-    const {user, headerHeight, introHeight, error, onCreatePlant} = this.props;
-    if (!user) return (<Redirect to={"/signup"}/>);
-    return (
-      <div className="container row mt-5 fullscreen">
-        <div className="mt-5 col-11 col-md-5 offset-1 offset-md-6">
-          <h2 className="mb-5"> Create a plant </h2>
-          <form onSubmit={onCreatePlant}>
-            <input className="mb-4" name="name" type="text" placeholder="Enter name"/>
-            <input className="mb-4" name="description" type="text" placeholder="Enter description"/>
-            <input className="mb-4 smallWidth" name="size" type="number" min="1" placeholder="Size"/> cm <br/>
-            <select className="mb-4 p-1" name="location" type="text">
-              <option> Select location </option>
-              <option value="sun"> sun </option>
-              <option value="shade"> shade </option>
-              <option value="sun and shade"> sun and shade </option>
-            </select>
-            <br/>
-            <input className="mb-4 smallWidth" name="price" type="number" min="1" placeholder="Price"/> € <br/>
-            <input className="mb-4" name="plantImage" type="file"/>
-            {
-              (error) ? <p className="warningColor"> {error} </p> : null            
-            }
-            <div className="col-12">
-              <button className="btn btn-sm btn-outline-dark" type="submit" > Create </button>
-              <Link to={"/"} onClick={() => scroll.scrollTo(headerHeight+introHeight)}> <button className="btn btn-sm mx-5"> Go back </button> </Link>
-            </div>
-          </form>
-        </div>
-      </div>
-    );
-  }
-}*/
-
 export default CreatePlantForm;
--- src/components/CreateRequestForm.js
+++ src/components/CreateRequestForm.js
@@ -1,8 +1,38 @@
-import React, {Component} from "react";
+import React, {useEffect} from "react";
 import {Link} from "react-router-dom";
 import {animateScroll as scroll} from "react-scroll";
 
-class CreateRequestForm extends Component {
+const CreateRequestForm = ({location, error, onCreateRequest, onResetError}) => {
+  useEffect(
+    () => {
+      onResetError();
+      scroll.scrollToTop();
+    },
+    []
+  );
+
+  const {plant} = location;
+  return (
+    <div className="container row mt-5">
+      <div className="mt-5 col-11 col-md-5 offset-1 offset-md-5">
+        <h2 className="mb-5"> Your mesage </h2>
+        <form onSubmit={(event) => onCreateRequest(event, plant)}>
+          <div>
+            <textarea className="mb-4" name="message" cols="35" rows="7"/>
+          </div>
+          {
+            error ? <p className="warningColor"> {error} </p> : null            
+          }
+          <button className="btn btn-sm btn-outline-dark" type="submit"> Send </button>
+          <Link to={`/plants/read/${plant._id}`}> <button className="btn btn-sm mx-2"> Go back </button> </Link>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+
+/*class CreateRequestForm extends Component {
 
   componentDidMount() {
     this.props.onResetError();
@@ -30,6 +60,6 @@ class CreateRequestForm extends Component {
       </div>
     );
   }
-}
+}*/
 
 export default CreateRequestForm;
