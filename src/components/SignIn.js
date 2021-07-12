eaccc45321a0c018bebcf3366d7d0e890cef5c39
christiangerbig <chr_gerbig@web.de>
2021-07-12 08:34:38 +0200

    classes to functions + hooks 2.5

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,39 +1,37 @@
-import React, {Component} from "react";
+import React, {useEffect} from "react";
 import {animateScroll as scroll} from "react-scroll";
 
-class SignIn extends Component {
-  
-  componentDidMount() {
-    const {onResetError, onResetNewRequestsReceived} = this.props;
-    onResetError();
-    onResetNewRequestsReceived();
-    scroll.scrollToTop();
-  }
+const SignIn = ({error, onSignIn, onResetError, onResetNewRequestsReceived}) => {
+  useEffect(
+    () => {
+      onResetError();
+      onResetNewRequestsReceived();
+      scroll.scrollToTop();
+    },
+    []
+  );
 
-  render() {
-    const {error, onSignIn} = this.props;
-    return (
-      <div className="container row mt-5 custom fullscreen">
-        <div className="mt-5 col-11 col-md-5 offset-1 offset-md-5">
-          <h2 className="mb-5"> Sign In </h2>
-          <form onSubmit={onSignIn}>
-            <div className="form-group">
-              <label htmlFor="InputEmail"> Email address </label>
-              <input type="email" className="form-control" id="InputEmail" name="email"/>
-            </div>
-            <div className="form-group">
-              <label htmlFor="InputPassword"> Password </label>
-              <input name="password" type="password" className="form-control" id="InputPassword"/>
-            </div>
-            {
-              (error) ? <p className="warningColor"> {error} </p> : null            
-            }
-            <button type="submit" className="btn btn-primary mt-4 btn-outline-dark"> Sign in </button>
-          </form>
-        </div>
+  return (
+    <div className="container row mt-5 custom fullscreen">
+      <div className="mt-5 col-11 col-md-5 offset-1 offset-md-5">
+        <h2 className="mb-5"> Sign In </h2>
+        <form onSubmit={onSignIn}>
+          <div className="form-group">
+            <label htmlFor="InputEmail"> Email address </label>
+            <input type="email" className="form-control" id="InputEmail" name="email"/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="InputPassword"> Password </label>
+            <input name="password" type="password" className="form-control" id="InputPassword"/>
+          </div>
+          {
+            error ? <p className="warningColor"> {error} </p> : null            
+          }
+          <button type="submit" className="btn btn-primary mt-4 btn-outline-dark"> Sign in </button>
+        </form>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default SignIn;
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,46 +1,44 @@
-import React, {Component} from "react";
+import React, {useEffect} from "react";
 import {Link} from "react-router-dom";
 import {animateScroll as scroll} from "react-scroll";
 
-class SignUp extends Component {
+const SignUp = ({error, onSignUp, onResetError, onResetNewRequestsReceived}) => {
+  useEffect(
+    () => {
+      onResetError();
+      onResetNewRequestsReceived();
+      scroll.scrollToTop();
+    },
+    []
+  );
 
-  componentDidMount() {
-    const {onResetError, onResetNewRequestsReceived} = this.props;
-    onResetError();
-    onResetNewRequestsReceived();
-    scroll.scrollToTop();
-  }
-
-  render() {
-    const {error, onSignUp} = this.props;
-    return (
-      <div className="container row mt-5">
-        <div className="mt-5 col-11 col-md-5 offset-1 offset-md-5">
-          <h2 className="mb-5"> Sign Up </h2>
-          <form onSubmit={onSignUp}>
-            <div className="form-group">
-              <label htmlFor="InputUsername"> Username </label>
-              <input type="text" className="form-control" id="InputUsername" name="username"/>
-            </div>
-            <div className="form-group">
-              <label htmlFor="InputEmail"> Email address </label>
-              <input type="email" className="form-control" id="InputEmail" name="email"/>
-            </div>
-            <div className="form-group">
-              <label htmlFor="InputPassword"> Password </label>
-              <input name="password" type="password" className="form-control" id="InputPassword"/>
-            </div>
-            {
-              (error) ? <p className="warningColor"> {error} </p> : null            
-            }
-            <button type="submit" className="btn btn-primary mt-4 btn-outline-dark"> Sign up </button>
-            <p className="padding"> Already have an account? </p>
-            <Link to={"/signin"}> Sign in </Link>
-          </form>
-        </div>
+  return (
+    <div className="container row mt-5">
+      <div className="mt-5 col-11 col-md-5 offset-1 offset-md-5">
+        <h2 className="mb-5"> Sign Up </h2>
+        <form onSubmit={onSignUp}>
+          <div className="form-group">
+            <label htmlFor="InputUsername"> Username </label>
+            <input type="text" className="form-control" id="InputUsername" name="username"/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="InputEmail"> Email address </label>
+            <input type="email" className="form-control" id="InputEmail" name="email"/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="InputPassword"> Password </label>
+            <input name="password" type="password" className="form-control" id="InputPassword"/>
+          </div>
+          {
+            error ? <p className="warningColor"> {error} </p> : null            
+          }
+          <button type="submit" className="btn btn-primary mt-4 btn-outline-dark"> Sign up </button>
+          <p className="padding"> Already have an account? </p>
+          <Link to={"/signin"}> Sign in </Link>
+        </form>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default SignUp;
