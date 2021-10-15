d573a33f46cd394707dd97d1e8cf2000867280fb
christiangerbig <chr_gerbig@web.de>
2021-10-15 09:20:52 +0200

    code cleanup 4.1

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -80,24 +80,20 @@ const NavBar = () => {
   useEffect(() => {
     if (loggedInUser) {
       dispatch(fetchAllMessages(isUserChange));
-      const currentAmountOfRequests = messages.filter(
-        (message: Message) => {
-          const { seller, messageState } = message;
-          return (
-            (seller as User)._id === loggedInUser._id && messageState === true
-          );
-        }
-      ).length;
+      const currentAmountOfRequests = messages.filter((message: Message) => {
+        const { seller, messageState } = message;
+        return (
+          (seller as User)._id === loggedInUser._id && messageState === true
+        );
+      }).length;
       if (amountOfRequests < currentAmountOfRequests) {
         dispatch(setAmountOfRequests(currentAmountOfRequests));
         dispatch(setIsNewRequest(true));
       }
-      const currentAmountOfReplies = messages.filter(
-        (message: Message) => {
-          const { buyer, reply } = message;
-          return (buyer as User)._id === loggedInUser._id && reply;
-        }
-      ).length;
+      const currentAmountOfReplies = messages.filter((message: Message) => {
+        const { buyer, reply } = message;
+        return (buyer as User)._id === loggedInUser._id && reply;
+      }).length;
       if (amountOfReplies < currentAmountOfReplies) {
         dispatch(setAmountOfReplies(currentAmountOfReplies));
         dispatch(setIsNewReply(true));
