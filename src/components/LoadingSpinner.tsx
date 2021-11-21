6e7b8e6fd37dbcd990fadd5662c87578d6eebc33
christiangerbig <chr_gerbig@web.de>
2021-11-21 20:54:26 +0100

    loading spinner 1.5

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -4,13 +4,14 @@ type LoadingSpinnerProps = {
 
 const LoadingSpinner = ({ spinnerText }: LoadingSpinnerProps): JSX.Element => {
   return (
-    <div
-      className="spinner-grow text-success mt-5 loadingSpinner"
-      role="status"
-    >
-      <span className="visually-hidden">
-        <br /> <br /> {spinnerText}
-      </span>
+    <div className="d-flex align-items-center justify-content-center">
+      <div
+        className="spinner-grow text-success m-3 loadingSpinner"
+        role="status"
+      ></div>
+      <div className="loadingSpinnerText">
+        <span> {spinnerText} </span>
+      </div>
     </div>
   );
 };
