69f29e4fed43c4e0281758477146ed13e2a03855
christiangerbig <chr_gerbig@web.de>
2021-11-24 20:25:14 +0100

    sass changed

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -2,7 +2,7 @@ const LoadingSpinner = (): JSX.Element => {
   return (
     <div className="d-flex align-items-center justify-content-center">
       <div
-        className="spinner-border m-3"
+        className="spinner-border m-3 loadingSpinner"
         role="status"
       ></div>
     </div>
