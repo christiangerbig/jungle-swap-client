6e38af530e98e60f960d734f5c9880d3551bc6a4
christiangerbig <chr_gerbig@web.de>
2021-12-16 10:33:04 +0100

    use of singleton <br> changed

--- src/screens/NotFound.tsx
+++ src/screens/NotFound.tsx
@@ -4,10 +4,8 @@ const NotFound = (): JSX.Element => {
   return (
     <div className="notFound">
       <div>
-        <h2>
-          Oh-oh! <br /> <br />
-          We think you got lost in the jungle!
-        </h2>
+        <h1> Oh-oh! </h1>
+        <h2> We think you got lost in the jungle! </h2>
         <h3> 404 Not Found </h3>
         <Link to={"/"}>
           <button className="btn btn-sm ml-2 smallWidth form-control">
--- src/screens/Unauthorized.tsx
+++ src/screens/Unauthorized.tsx
@@ -1,11 +1,9 @@
 const Unauthorized = (): JSX.Element => {
   return (
-    <div className="notFound">
+    <div className="unauthorized">
       <div>
-        <h2>
-          Oh-oh! <br /> <br />
-          We think you got lost in the jungle!
-        </h2>
+        <h1> Oh-oh! </h1>
+        <h2> We think you got lost in the jungle! </h2>
         <h3> 401 Unauthorized </h3>
       </div>
     </div>
