9e04cdae0a880af7e71c023cdaa26afb2c9a6782
christiangerbig <chr_gerbig@web.de>
2022-10-12 19:18:59 +0200

    eager loading for main image added

--- src/components/home/HomeAbout.tsx
+++ src/components/home/HomeAbout.tsx
@@ -11,7 +11,7 @@ const HomeAbout = (): JSX.Element => {
         <div className="col-sm-6 col-md-5 col-lg-6">
           <img
             src={plantsImage}
-            loading="lazy"
+            loading="eager"
             alt="plants"
             className="is-background-image"
           />
