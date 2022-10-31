5c35e0becd1bb416558b133d6a32af927f6aae9b
christiangerbig <chr_gerbig@web.de>
2022-10-31 20:32:18 +0100

    i18n - json file included

--- src/app/i18n.ts
+++ src/app/i18n.ts
@@ -1,15 +1,15 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import en from "./locales/en";
+import en from "./locales/en.json";
 
 i18n.use(initReactI18next).init({
   resources: {
-    en: en,
+    en,
   },
   fallbackLng: "en",
   debug: false,
-  ns: ["translations"],
-  defaultNS: "translations",
+  ns: ["translation"],
+  defaultNS: "translation",
   interpolation: {
     escapeValue: false,
   },
