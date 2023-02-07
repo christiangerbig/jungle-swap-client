a12302d42c66a5a06ced5294bc26bf2e7aa06f37
christiangerbig <chr_gerbig@web.de>
2023-02-07 15:59:13 +0100

    code cleanup 8.9

--- src/views/apis/PaymentCheckoutView.tsx
+++ src/views/apis/PaymentCheckoutView.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Redirect } from "react-router-dom";
 import { animateScroll as scroll } from "react-scroll";
 import { useAppSelector } from "../../app/hooks";
@@ -8,11 +8,12 @@ import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import PaymentCheckoutForm from "../../components/apis/PaymentCheckoutForm";
 
-const stripePromise = loadStripe(
-  "pk_test_51IQBsPA6EAM4YnfDyrjHWnLHzZ5KkI9tsERzYhBGVoctZBrFUb4Sda035HvcQKpp7thFiqW6QmO8ytPbOAMTg33z00cHvcbojv"
-);
-
 const PaymentCheckoutView = (): JSX.Element => {
+  const [stripePromise] = useState(() =>
+    loadStripe(
+      "pk_test_51IQBsPA6EAM4YnfDyrjHWnLHzZ5KkI9tsERzYhBGVoctZBrFUb4Sda035HvcQKpp7thFiqW6QmO8ytPbOAMTg33z00cHvcbojv"
+    )
+  );
   const loggedInUser = useAppSelector(selectLoggedInUser);
   const { protectRoute } = useRouting();
   const { scrollToTop } = scroll;
