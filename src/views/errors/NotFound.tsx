df078269a2a879e59807679c696be94adb93a4ca
christiangerbig <chr_gerbig@web.de>
2023-03-08 20:26:23 +0100

    BEM classes added 2.3

--- src/views/errors/NotFound.tsx
+++ src/views/errors/NotFound.tsx
@@ -5,13 +5,34 @@ const NotFound = (): JSX.Element => {
   const { t } = useTranslation();
 
   return (
-    <div className="not-found has-background-image2">
+    <div
+      className={`
+        [
+          not-found-container 
+        ]
+        [
+          position-absolute 
+          d-flex 
+          justify-content-center 
+          align-items-center 
+          w-100  
+          overflow-hidden
+          text-center
+        ]
+      `}
+    >
       <div>
-        <h1>{t("texts.notFound.headline")}</h1>
-        <h2>{t("texts.notFound.subheadline1")}</h2>
-        <h3>{t("texts.notFound.subheadline2")}</h3>
-        <Link to={"/"} className="is-link">
-          <button className="mx-2 btn btn-sm is-width-medium form-control">
+        <h1 className="not-found-container__headline">
+          {t("texts.notFound.headline")}
+        </h1>
+        <h2 className="[ not-found-container__subheadline1 ] [ mb-4 }">
+          {t("texts.notFound.subheadline1")}
+        </h2>
+        <h3 className="[ not-found-container__subheadline2 ] [ text-uppercase mb-4 ]">
+          {t("texts.notFound.subheadline2")}
+        </h3>
+        <Link to={"/"} className="navigation-link">
+          <button className="[ button--width-medium ] [ btn btn-sm form-control px-3 ]">
             {t("button.takeMeHome")}
           </button>
         </Link>
