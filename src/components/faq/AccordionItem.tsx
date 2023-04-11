b2fa01a282a410c321389efd99f791872d5fed0d
Christian Gerbig <christian.gerbig@pushh.it>
2023-04-11 12:14:46 +0200

    Accordion-Component style classes added

--- src/components/faq/AccordionItem.tsx
+++ src/components/faq/AccordionItem.tsx
@@ -40,7 +40,7 @@ const AccordionItem = ({ header, body }: AccordionItemProps): JSX.Element => {
       <div className={`accordion-item__header ${setHeaderActive}`}>
         <button
           type="button"
-          className="accordion-item__header__button"
+          className="accordion-item__header__button d-flex flex-row justify-content-between align-items-start w-100 text-left"
           onClick={handleClickButton}
         >
           <h4 className="accordion-item__header__text">{header}</h4>
@@ -48,14 +48,14 @@ const AccordionItem = ({ header, body }: AccordionItemProps): JSX.Element => {
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 512 412"
-              className={`accordion-item__header__imagebox__image ${setImageActive}   w-100 h-100 `}
+              className={`accordion-item__header__imagebox__image ${setImageActive} w-100 h-100 `}
             >
               <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
             </svg>
           </div>
         </button>
       </div>
-      <div ref={divBodyRef} className="accordion-item__body">
+      <div ref={divBodyRef} className="accordion-item__body pt-0 ppb-1 px-1">
         <p>
           {body}
           <br />
