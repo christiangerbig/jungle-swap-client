c868581597f88be84e48ab5247d76af1e8b65d40
Christian Gerbig <christian.gerbig@pushh.it>
2023-04-12 08:46:10 +0200

    FAQ view layout changed

--- src/views/faq/FaqView.jsx
+++ src/views/faq/FaqView.jsx
@@ -11,7 +11,7 @@ const FaqView = () => {
       <div className="col-12 col-md-6 offset-md-4 mt-5">
         <h2 className="text-left mb-5">{t("texts.faq.overview.headline")}</h2>
       </div>
-      <div className="col-12 col-md-6 offset-md-4 mt-5">
+      <div className="col-12 col-md-6 offset-md-4">
         <Accordion data={faqData} />
       </div>
     </div>
